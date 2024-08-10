2a552870e8cb4f0c6d7aff5d7ba14b024403cc4d
Junki Kwak <junkwak@c3r10s4.42gyeongsan.kr>
2024-08-10 23:52:39 +0900

    v

--- pipex/main.c
+++ pipex/main.c
@@ -20,6 +20,7 @@ char	**ft_parsing(char **envp)
 	while (envp[i])
 	{
 		if (ft_strnstr(envp[i], "PATH=", 5))
+			//printf("now envp: %s\n", envp[i]);
 			return (ft_ppsplit(envp[i] + 5, ':'));
 		i++;
 	}
@@ -38,7 +39,7 @@ char	*ft_check(char **path, char *cmd)
 	{
 		find = ft_strjoin(path[i], "/");
 		full_cmd = ft_strjoin(find, cmd);
-		if (access(find, X_OK) == 0)
+		if (access(full_cmd, F_OK) == 0)
 			return (full_cmd);
 		free(full_cmd);
 		i++;
@@ -51,27 +52,39 @@ void	ft_error(char *s)
 	exit(1);
 }
 
-void	fc_ps(t_arg *pipex, int *fd, char **argv, char **envp)
+void	fc_ps(t_arg *pipex, int *fd, char **argv)
 {
+	char	*f = NULL;
 	pipex -> f_content = open(argv[1], O_RDONLY, 0644);
+	// printf("path : %d\n", pipex -> f_content);
 	if (pipex -> f_content == -1)
 		ft_error("child error f.c_ps1");
+	// printf("path : %s\n", pipex -> path[0]);
 	pipex -> order1 = ft_check(pipex -> path, pipex -> cmd1[0]);
-	if (!pipex -> order1)
+	if (!(pipex -> order1))
 		ft_error("command not found fc_ps1-1");
+	//printf("order1 : %s\ncmd1 : %s\n", pipex -> order1, pipex-> cmd1[0]);
+	//printf("order1 : %s\ncmd1 : %s\n", pipex -> path[0], pipex-> cmd1[0]);
 	close(fd[0]);
-	if (dup2(pipex -> f_content, 0) == -1)
-		ft_error("dup2 error f.c_ps 2");
-	if (dup2(fd[1], 1) == -1)
-		ft_error("dup2 error f.c_ps 3");
+	if (dup2(pipex -> f_content, 0) == -1)   //dup2(pipex -> s_content, 0) (fd[1], 0)
+		ft_error("dup2 error fc 2");
+	if (dup2(fd[1],1) == -1) // (pipex -> s_content, 1)
+		ft_error("dup2 error fc3");
+	// if (dup2(pipex -> f_content, 0) == -1)
+	// 	ft_error("dup2 error f.c_ps 2");
+	// if (dup2(fd[1], 1) == -1)
+	// 	ft_error("dup2 error f.c_ps 3");
 	close(fd[1]);
 	close(pipex -> f_content);
-	if (execve(pipex -> order1, pipex -> cmd1, envp) == -1)
+	if (execve(pipex -> order1, pipex -> cmd1, &f) == -1)
+	//char	*prog = "/bin/ls";
+	//char	*pass[] = {"ls", NULL};
 	       ft_error("exec error f.c 4");
 }	
 		
-void	sc_ps(t_arg *pipex, int *fd, char **argv, char **envp)
+void	sc_ps(t_arg *pipex, int *fd, char **argv)
 {
+	//char	*f = NULL;
 	pipex -> s_content = open (argv[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (pipex -> s_content == -1)
 		ft_error("child error sc 1");
@@ -79,13 +92,13 @@ void	sc_ps(t_arg *pipex, int *fd, char **argv, char **envp)
 	if (!pipex -> order2)
 		ft_error("command not found sc1-1");
 	close(fd[1]);
-	if (dup2(pipex -> s_content, 0) == -1)   //fd[0]
+	if (dup2(fd[0], 0) == -1)   //dup2(pipex -> s_content, 0) 
 		ft_error("dup2 error sc 2");
-	if (dup2(fd[0], 1) == -1) // pipex -> s_content
+	if (dup2(pipex -> s_content, 1) == -1) // fd[0]
 		ft_error("dup2 error sc3");
 	close(fd[0]);
 	close(pipex -> s_content);
-	if (execve(pipex -> order2, pipex -> cmd2, envp) == -1)
+	if (execve(pipex -> order2, pipex -> cmd2, NULL) == -1)
 		ft_error("exec error sc4");
 }
 
@@ -95,19 +108,21 @@ void	ft_make_pipex(t_arg *pipex, char **argv, char **envp)
 	pid_t	pid2;
 	int	fd[2];
 	
+	if (!envp[0][0])
+		return ;
 	pipe(fd);
 	pid1  = fork();
 	if (pid1 == -1)
 		ft_error("PID error");
 	else if (pid1 == 0)
-		fc_ps(pipex, fd, argv, envp);
+		fc_ps(pipex, fd, argv);
 	else
 	{
 		pid2 = fork();
 		if (pid2 == -1)
 			ft_error("PID error");
 		else if (pid2 == 0)
-			sc_ps(pipex, fd, argv, envp);
+			sc_ps(pipex, fd, argv);
 		else
 		{
 			close(fd[0]);
